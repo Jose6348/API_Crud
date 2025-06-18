9c815d189fed9c197ae7548f027ca8f67f7efe8e
José Jorge Falasco <jose.falasco@datasystem.com.br>
2025-06-18 14:34:15 -0300

    Endpoints implementados

---
+++ Comunication/Responses/ResponseRegisterUserJson.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Crud.Comunication.Responses
+{
+    public class ResponseRegisterUserJson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API_Crud.Comunication.Requests;
+using API_Crud.Comunication.Responses;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API_Crud.Controllers
 {
@@ -6,18 +8,63 @@ namespace API_Crud.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+
+        // Requisição do tipo Get 
+
         [HttpGet]
-        [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
-        public IActionResult Get()
+        public IActionResult GetById([FromRoute] int id)
         {
-            var response = new Response
+            var response = new User
             {
+                Id = 1,
                 Name = "Jorge",
                 Age = 30
             };
 
             return Ok(response);
         }
+
+        // Requisição do tipo Post
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+        public IActionResult Create([FromBody] RequestRegisterUserJson request)
+        {
+            var response = new ResponseRegisterUserJson
+            {
+                Id = 1,
+                Name = request.Name
+            };
+
+            return Created(string.Empty, response);
+        }
+
+    // Requisição do tipo Put
+
+        [HttpPut]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+        public IActionResult Update(
+            [FromRoute] int id,
+            [FromBody] RequestUpdateUserProfileJson request)
+        {
+            return NoContent();
+        }
+
+        // Requisição do tipo Delete
+
+        [HttpDelete]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+        public IActionResult Delete()
+        {
+            return NoContent();
+        }
+
     }
 }
